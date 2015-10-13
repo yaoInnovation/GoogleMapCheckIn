a821c5a72427892a4b0902296a8069200e258345
yao zhou <yaozhou@andrew.cmu.edu>
2015-10-13 01:26:44 -0400

    both search and text change work with marker

--- IMHere/IMHere/static/IMHere/js/home.js
+++ IMHere/IMHere/static/IMHere/js/home.js
@@ -1,6 +1,52 @@
-window.onload = function() {
+var map;
+var marker;
+var searchBox;
+var service;
+var curPosition;
 
-	var map = new google.maps.Map(document.getElementById('map-canvas'), {
+window.onload = function() {
+	createMap();
+}
+
+function PinButtonClick() {
+	if(navigator.geolocation) {
+	  navigator.geolocation.getCurrentPosition(pinCallBack);
+	} else {
+	  error('Geo Location is not supported');
+	}
+}
+
+function pinCallBack(position) {
+	curPosition = position;
+	var coords = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+	var options = {
+	zoom: 15,
+	center: coords,
+	mapTypeControl: false,
+	navigationControlOptions: {
+		style: google.maps.NavigationControlStyle.SMALL
+	},
+	mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	map = new google.maps.Map(document.getElementById("map-canvas"), options);
+
+	marker = new google.maps.Marker({
+	  position: coords,
+	  map: map
+	});
+}
+
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+}
+
+function createMap() {
+	map = new google.maps.Map(document.getElementById('map-canvas'), {
 		center: {
 			lat:27.72,
 			lng:85.36
@@ -8,7 +54,7 @@ window.onload = function() {
 		zoom:15
 	});
 
-	var marker = new google.maps.Marker({
+	marker = new google.maps.Marker({
 		position: {
 			lat: 27.72,
 			lng: 85.36
@@ -17,7 +63,7 @@ window.onload = function() {
 		draggable:false
 	});
 
-	var searchBox = new google.maps.places.SearchBox(document.getElementById('AddressInput'));
+	searchBox = new google.maps.places.SearchBox(document.getElementById('AddressInput'));
 
 	// Place change event on search box
 	google.maps.event.addListener(searchBox, 'places_changed', function() {
